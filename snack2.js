e03c4542ce11eaf52f4727607037081b39cf1d84
Shikanto <antonello.anzini.comunicazioni@gmail.com>
2021-10-25 18:27:57 +0200

    ex teams done

--- snack2.js
+++ snack2.js
@@ -20,3 +20,28 @@ const footballTeams = [
         puntiFatti: 0
     },
 ]
+
+function randomInfo(footballTeams){
+
+    let updateTeams =[];
+    let newInfoTeam ={
+
+    };
+
+    for( let i = 0; i < footballTeams.length; i++) {
+
+        let teamInfo = footballTeams[i];
+        teamInfo.falliSubiti = Math.floor(Math.random()*100);
+        teamInfo.puntiFatti = Math.floor(Math.random()*100);
+        /* console.log(teamInfo) */
+        let {nome, falliSubiti} = teamInfo;
+        newInfoTeam.nome = nome;
+        newInfoTeam.falliSubiti = falliSubiti;
+        /* console.log(newInfoTeam); */
+        updateTeams.push(newInfoTeam);
+        
+    }
+    console.log(updateTeams)
+}
+
+randomInfo(footballTeams);
